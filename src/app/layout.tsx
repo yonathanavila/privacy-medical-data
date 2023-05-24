375bbfd2c191f6f62c1b9a8198d1371a6c3993e6
yonathanwemerge <mcflymarty412@gmail.com>
2023-05-23 23:24:58 -0600

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
-import Provider from './Provider'
+import './globals.css';
+import Script from 'next/script';
+import Provider from './Provider';
+import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,7 +18,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">s
+    <html lang="en">
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-JTMGTW1JXZ" />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                    window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-JTMGTW1JXZ', {
+                        page_path: window.location.pathname,
+                      });
+            `,
+        }}
+      />
       <body className='bg-gradient-to-b from-[#131727] to-[#070816] h-max'>
         <div className="bck h-max"></div>
         <div className="dotBck h-max" style={{ transform: "scale(1.1)" }}></div>
